4bb7fc09be750a9ce49cbff80a42fc939776f0ce
UcheTheo <theo.uche2023@gmail.com>
2024-05-11 01:27:50 +0100

    Create the necessary route pages and included it in the routes

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-import { Header } from "../components";
+import React, { Suspense } from "react";
+import { Header, MainSpinner } from "../components";
+import { Route, Routes } from "react-router-dom";
+import { HomeContainer } from "../containers";
+import {CreateResume, CreateTemplate, TemplateDesignPinDetail, UserProfile} from "../pages";
 
 const HomeScreen = () => {
   return <div className="w-full flex flex-col items-center justify-center">
     {/* header */}
     <Header />
-    <main>
+    <main className="w-full">
         {/* custom routes */}
+        <Suspense fallback={<MainSpinner />}>
+            <Routes>
+                <Route path="/" element={<HomeContainer />} />
+                <Route path="/template/create" element={<CreateTemplate />} />
+                <Route path="/profile/:uid" element={<UserProfile />} />
+                <Route path="/resume/*" element={<CreateResume />} />
+                <Route 
+                    path="/resumeDetail/:templateID" 
+                    element={<TemplateDesignPinDetail />} 
+                />
+            </Routes>
+        </Suspense>
     </main>
   </div>;
 };
